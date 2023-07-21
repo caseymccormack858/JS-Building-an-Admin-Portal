03037f3f6796cdd7f65a134c9512589e6d304d38
Casey McCormack <caseymccormack858@gmail.com>
2023-07-21 11:07:16 -0700

    final update

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,11 @@
-
-// Your Code Here
+let response = await fetch('http://localhost:9001/updateBook',{
+    method: "PATCH",
+    headers: {
+        'Content-Type': 'application/json'
+    });
+    body: JSON.stringify ({
+        "id": 3,
+        "title": "Legends of Arathrae",
+    }),
+let updatebook = await response.json();
+console.log(updatedbook);
